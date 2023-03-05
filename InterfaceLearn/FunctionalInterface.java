ef7622c8fe6c33e107c83765a2f06456070c808d
Sahil <60694942+dhillonsahil@users.noreply.github.com>
2023-03-05 20:20:24 +0530

    functional Interface

---
+++ InterfaceLearn/FunctionalInterface.java
@@ -0,0 +1,13 @@
+package InterfaceLearn;
+public class FunctionalInterface {
+    public static void main(String[] args) {
+        
+    }
+}
+interface Student{
+    void hello();
+    
+}
+abstract class Hey{
+    private String name;
+}
