ed7df82dee5ff3d27bb06ba5238af26da25954ee
Sahil <60694942+dhillonsahil@users.noreply.github.com>
2023-03-22 17:41:28 +0530

    PriorityQueue

---
+++ LearnCollection/Launch_PQ1.java
@@ -0,0 +1,26 @@
+package LearnCollection;
+
+import java.util.*;
+
+public class Launch_PQ1 {
+    public static void main(String[] args) {
+        PriorityQueue pq1 = new PriorityQueue();
+        // pq1.add(10);
+        // pq1.add(2);
+        // pq1.add(20);
+        // pq1.add(50);
+        // pq1.add(45);
+        // pq1.add(41);
+        // pq1.add(75);
+        // pq1.add(74);
+        pq1.add(100);
+        pq1.add(50);
+        pq1.add(150);
+        pq1.add(25);
+        pq1.add(75);
+        pq1.add(125);
+        pq1.add(175);
+        pq1.add("PW");
+        System.out.println(pq1);
+    }
+}
