861ce06d15151895146d3be5ad5bcd687ac021a5
Sahil <sahil@kali.domain.name>
2023-04-05 06:32:59 -0400

    date and Time api

---
+++ Api/DisplayTime.java
@@ -0,0 +1,20 @@
+import java.time.*;
+import java.util.Date;
+import java.sql.Date;
+
+public class DisplayTime {
+    public static void main(String[] args) {
+        // Method 1
+        LocalDate dt1 =  LocalDate.now();
+        System.out.println(dt1);
+
+        // Type 2
+        Date dt = new Date();
+        System.out.println(dt);
+
+        // Type 3
+        java.sql.Date dt2 = new java.sql.Date(dt.getTime());
+        System.out.println(dt2);
+
+    }
+}
