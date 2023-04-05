793a33c0770f17a7dd961c9c1a5a24b66d1eb30b
Sahil <sahil2742002@gmail.com>
2023-04-05 06:43:28 -0400

    date and Time api

--- Api/DisplayTime.java
+++ Api/DisplayTime.java
@@ -1,15 +1,16 @@
+package Api;
 import java.time.*;
 import java.util.Date;
-import java.sql.Date;
 
 public class DisplayTime {
     public static void main(String[] args) {
         // Method 1
         LocalDate dt1 =  LocalDate.now();
         System.out.println(dt1);
-
+        
         // Type 2
         Date dt = new Date();
+        System.out.println(dt.getTime());
         System.out.println(dt);
 
         // Type 3
